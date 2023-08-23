5ba59c90987ab46a6b5d1dd3e410596b37164a29
BilkisAkterDalia <bilkisadalia@gmail.com>
2023-08-23 22:21:31 +0600

    Module

---
+++ Module-12/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+cout<<"This"<<endl;
+        for(int i=l;i<=mid;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=mid+1;i<=r;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        
